40cff432474f22eb112912e616255753a40ecd0e
Joseph  Borne <bourne_joseph_pierre@student.smc.edu>
2022-09-24 14:52:10 +0000

    FINISHED part 1 of my assignment module 3 lab.

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ $('#getWeatherBtn').click(function() {
   var city = $('#zipCode').val();
   $.ajax({
     url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city +
-      '&units=metric' + "&appid=000c53231273df2ac9323e70eee830d4",
+      ',us&units=metric' + "&appid=000c53231273df2ac9323e70eee830d4",
     type: 'get',
     dataType: "jsonp",
     success: function(data) {
-      console.log(data);
-      alert(data.name);
+      console.log(data); //testing
+      $('#locationName').html(data.name);
+      $('#condition').html(data.weather[0].description);
+      $('#currentTemp').html(data.main.temp);
+
+      $('#minTemp').html(data.main.temp_min);
+      $('#maxTemp').html(data.main.temp_max);
+      $('#windSpeed').html(data.wind.speed);
+      $('#humidity').html(data.main.humidity);
+      $('#pressure').html(data.main.pressure);
     }
   })
 })
